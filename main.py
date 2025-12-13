3f98c6e4dd28413329364231fb2a96d914c933c9
mandelbrojt <luis.moreno@exatec.tec.mx>
2025-12-13 03:42:33 +0000

    Add sys arguments

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+import sys
+from stats import count_words, count_characters, sort_char_count
+
 def get_book_text(path):
     with open(path, "r") as f:
         return f.read()
 
 
-def count_words(book_text):
-    words_list = book_text.split()
-    return len(words_list)
-
-
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_filepath = f"./{sys.argv[1]}"
+    book_text = get_book_text(book_filepath)
     book_words_count = count_words(book_text)
+    counted_chars = count_characters(book_text)
+    sorted_chars = sort_char_count(counted_chars)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_filepath[2:]}...")
+    print("----------- Word Count ----------")
     print(f"Found {book_words_count} total words")
+    print("--------- Character Count -------")
+    for dictionary in sorted_chars:
+        if dictionary["char"].isalpha():
+            print(f"{dictionary['char']}: {dictionary['num']}")
+    print("============= END ===============")
 
 
 main()
